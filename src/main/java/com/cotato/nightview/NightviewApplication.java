d344da01d7b405943a8c923a0fe49268717e533e
김민욱 <qjvk2880@naver.com>
2023-01-20 16:18:09 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/com/cotato/nightview/NightviewApplication.java
@@ -0,0 +1,13 @@
+package com.cotato.nightview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotato/nightview/NightviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotato.nightview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
