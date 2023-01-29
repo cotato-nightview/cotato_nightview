256528061122c865d566c68150f566686d1e6561
qjvk2880 <qjvk2880@gmail.com>
2023-01-30 00:58:59 +0900

    ✨ model mapper 추가

---
+++ src/main/java/com/cotato/nightview/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.cotato.nightview.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
--- src/main/java/com/cotato/nightview/place/PlaceViewController.java
+++ src/main/java/com/cotato/nightview/place/PlaceViewController.java
@@ -1,6 +1,8 @@
 package com.cotato.nightview.place;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
@@ -17,6 +19,7 @@ import java.util.List;
 @PropertySource("classpath:config.properties")
 public class PlaceViewController {
     private final PlaceService placeService;
+    private final ModelMapper modelMapper;
 
     //    default map
     @GetMapping("/map")
@@ -26,6 +29,17 @@ public class PlaceViewController {
 
         List<Place> placeList = placeService.findAll();
         model.addAttribute("placeList", placeList);
+
+        TypeMap<Place, PlaceDto> typeMap = modelMapper.createTypeMap(Place.class, PlaceDto.class);
+        typeMap.addMappings(mapper -> {
+            mapper.map(Place::getLatitude, PlaceDto::setMapy);
+            mapper.map(Place::getLongitude, PlaceDto::setMapx);
+        });
+//        modelMapper.createTypeMap(Place.class,PlaceDto.class)
+//                .addMapping(Place::getLongitude,PlaceDto::setMapy)
+//                .addMapping(Place::getLatitude,PlaceDto::setMapy);
+        PlaceDto map = modelMapper.map(defaultPlace, PlaceDto.class);
+        System.out.println(map.toString());
         return "map/map";
     }
 }
